9c382cd2fe3102eec069a9b5760d1ddbd822fdbd
ArunaSolanki03 <arunasolanki305@gmail.com>
2026-04-21 07:47:30 +0530

    Add files via upload

---
+++ Program to check membership in a list.py
@@ -0,0 +1,6 @@
+#program to check membershipin a list
+numbers=[10,20,30,40,50]
+#check if 20 is in the list
+print("Is 20 in list?",20 in numbers)
+#check if 60 is not in the list
+print("Is 60 in list?",60 in numbers)
