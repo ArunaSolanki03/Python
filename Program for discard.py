88a792ca85d3e26513c4a5591522f834be3424b4
ArunaSolanki03 <arunasolanki305@gmail.com>
2026-05-14 09:52:43 +0530

    Add files via upload

---
+++ Program for discard.py
@@ -0,0 +1,9 @@
+months={"Jan","Feb","Mar","Apr"}
+print("\n Printing the original set:")
+print(months)
+months.discard("Apr")
+print("\n Printing the modified set...")
+print(months)
+months.discard("May")
+print("\n Printing the modified set...")
+print(months)
