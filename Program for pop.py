832b04dcaf80f02707d316e5d2032642e0c3e14d
ArunaSolanki03 <arunasolanki305@gmail.com>
2026-05-19 10:37:18 +0530

    Add files via upload

---
+++ Program for pop.py
@@ -0,0 +1,11 @@
+Days={"Mon","Tue","Wed"}
+print(f"Printing the original set: {Days}")
+pop_value=Days.pop()
+print(f"Popped value: {pop_value}")
+print(f"Printing the modified set: {Days}");
+pop_value=Days.pop()
+print(f"Popped value: {pop_value}")
+print(f"Printing the modified set: {Days}")
+Days=set()
+pop_value=Days.pop()
+print("Popped value is:",pop_value)
